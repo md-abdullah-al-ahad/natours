671bc5ba99803aba58238b313f3e8989fea600e8
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-12-26 18:59:55 +0600

    add post functionality in app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
